7aad2863d3de556ff29f46e7954089d167f51c3b
NiverMtz <nivermartinezmed@gmail.com>
2020-03-20 07:42:51 -0600

    NO he corregido el slinder de imagenes

---
+++ js/slider.js
@@ -0,0 +1,41 @@
+var slider = ('#slider');
+var sig = $('#btn-next');
+var ante = $('#btn-prev');
+
+$('#slider section:last').insertBefore('#slider seccion:first');
+
+slider.css('margin-left', '-'+100+'%');
+
+function moverD() {
+    slider.animate({
+    marginleft:'-'+200+'%'
+    } ,700, function() {
+        $('#slider section:fisrt').insertAfter('#slider section:last');
+        slider.css('margin-left', '-'+100+'%');
+    });
+}
+
+sig.on('click', function() {
+    moverD();
+});
+
+function moverI() {
+    slider.animate({
+    marginleft:0
+    } ,700, function() {
+        $('#slider section:last').insertBefore('#slider section:first');
+        slider.css('margin-left', '-'+100+'%');
+    });
+}
+
+ante.on('click', function() {
+    moverI();
+});
+
+function autoplay() {
+    interval = setInterval(function(){
+        moverD;
+    }, 50000);
+}
+
+autoplay();
